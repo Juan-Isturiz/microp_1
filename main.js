6c91a4e441a77f8630f0f00e56ccfccedcfcd182
Juan Isturiz <juan.isturiz@correo.unimet.edu.ve>
2022-02-12 13:40:39 -0400

    primer comit, carrusel y navbar a medias

---
+++ main.js
@@ -0,0 +1,30 @@
+//let slideIndex = 1;
+//const showSlides = (n) => {
+//  let i;
+//  let slides = document.getElementsByClassName("mySlides");
+//  if (n > slides.length) { slideIndex = 1 }
+//  if (n < 1) { slideIndex = slides.length }
+//  for (i = 0; i < slides.length; i++) {
+//    slides[i].style.display = "none";
+//  }
+//  slides[slideIndex - 1].style.display = "block";
+//}
+//showSlides(slideIndex);
+//
+//const plusSlides = (n) => {
+//  showSlides(slideIndex += n);
+//}
+//const currentSlide = (n) => {
+//  showSlides(slideIndex = n);
+//}
+
+const about = () => {
+  const aboutPage = document.getElementById("text")
+  if (aboutPage.style.display === 'none') {
+    aboutPage.setAttribute("style", "display:flex")
+    document.getElementById("header").setAttribute("class", "header__about")
+  } else{
+    aboutPage.style.display = "none";
+    document.getElementById("header").setAttribute("class", "header")
+  }
+}
